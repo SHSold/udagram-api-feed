438f4869e4724fba78b12c67d74d13ce2d07a127
Stefan Sold <ssold@web.de>
2021-12-30 12:17:00 +0100

    ci/cd version causes problems...?

--- src/controllers/v0/feed/routes/feed.router.ts
+++ src/controllers/v0/feed/routes/feed.router.ts
@@ -20,7 +20,7 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
   const token = tokenBearer[1];
   return jwt.verify(token, c.config.jwt.secret, (err, decoded) => {
     if (err) {
-      return res.status(500).send({auth: false, message: 'Failed to authenticate.'});
+      return res.status(500).send({auth: false, message: 'Failed to authenticate. Dudel Dadel Dieeee'});
     }
     return next();
   });
