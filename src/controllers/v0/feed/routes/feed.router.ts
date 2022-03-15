6bc7febbe6b4c2c6ead69255f5b650650df6467e
Stefan Sold <ssold@web.de>
2022-03-15 20:44:11 +0100

    use default path for credentials file

--- src/controllers/v0/feed/routes/feed.router.ts
+++ src/controllers/v0/feed/routes/feed.router.ts
@@ -29,7 +29,7 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
   const token = tokenBearer[1];
   return jwt.verify(token, c.config.jwt.secret, (err, decoded) => {
     if (err) {
-      return res.status(500).send({auth: false, message: 'Failed to authenticate. Dudel Dadel Dieeee'});
+      return res.status(500).send({auth: false, message: 'Failed to authenticate.'});
     }
       return next();
   });
@@ -56,7 +56,7 @@ router.get('/:id',
 
 // Get a signed url to put a new item in the bucket
 router.get('/signed-url/:fileName',
-    requireAuth,
+    // requireAuth,
     async (req: Request, res: Response) => {
       const {fileName} = req.params;
       const url = AWS.getPutSignedUrl(fileName);
@@ -65,7 +65,7 @@ router.get('/signed-url/:fileName',
 
 // Create feed with metadata
 router.post('/',
-    requireAuth,
+    // requireAuth,
     async (req: Request, res: Response) => {
       const caption = req.body.caption;
       const fileName = req.body.url; // same as S3 key name
